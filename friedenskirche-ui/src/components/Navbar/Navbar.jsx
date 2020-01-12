ee2cb00f0df3f62fbe6042eed4acf1651dcefe0a
Paul Finkbeiner <paul-d-f@web.de>
2020-01-12 16:31:17 +0100

    small changes to navbar

--- friedenskirche-ui/src/components/Navbar/Navbar.jsx
+++ friedenskirche-ui/src/components/Navbar/Navbar.jsx
@@ -10,9 +10,11 @@ import '../Navbar/Navbar.scss';
 const Navbar = () => {
     return (
         <nav className="nav-container">
+
             <Link className="logo-link" to="Gemeinde">
                 <img className="logo" src={logo} alt="logo"/>
             </Link>
+
 
             <Link className="navbar-item" to="Gemeinde">Gemeinde</Link>
             <Link className="navbar-item" to="Aktuell">Aktuell</Link>
